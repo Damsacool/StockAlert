626d16f53f47028c0af8395ed190705c0895fc1f
Akande Samad <akandesamad26@gmail.com>
2026-03-03 10:51:34 +0000

    Fix: Force service worker update on all devices

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 // StockAlert Service Worker - Offline First
-const CACHE_NAME = 'stockalert-v3';
+const CACHE_NAME = 'stockalert-v4';
 
 // Files to cache on install
 const PRECACHE_URLS = [
