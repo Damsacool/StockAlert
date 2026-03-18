7c01cff43a65bbf6d4b5d6f96c24c591de07a497
Akande Samad <akandesamad26@gmail.com>
2026-03-18 23:37:20 +0000

    Fix deprecation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 // StockAlert Service Worker - Offline First
-const CACHE_NAME = 'stockalert-v6';
+const CACHE_NAME = 'stockalert-v7';
 
 // Files to cache on install
 const PRECACHE_URLS = [
