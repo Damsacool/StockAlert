39f7f71d0cbf3fb874714682476a5e5884ab97e3
Akande Samad <akandesamad26@gmail.com>
2026-03-26 00:13:24 +0000

    incremented version to force update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 // StockAlert Service Worker - Offline First
-const CACHE_NAME = 'stockalert-v8';
+const CACHE_NAME = 'stockalert-v9'; 
 
 // Files to cache on install
 const PRECACHE_URLS = [
